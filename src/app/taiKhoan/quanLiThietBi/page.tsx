afe48ff389583bcb85e63ebe982d349eda5992be
Chí Thành <chithanh171004@gmail.com>
2024-07-01 17:17:13 +0700

    quanLiThietBi

--- src/app/taiKhoan/quanLiThietBi/page.tsx
+++ src/app/taiKhoan/quanLiThietBi/page.tsx
@@ -1,8 +1,13 @@
 import { auth } from "@/_libs/auth";
-import { DeviceInfo } from "../../../_components/DeviceInfo";
+import { DeviceInfo } from "@/_components/DeviceInfo";
 
 export default async function page() {
   const session = await auth();
 
-  return <DeviceInfo session={session} />;
+  return (
+    <div>
+      <h1 className="text-2xl font-bold mb-6">Thiết bị đã đăng nhập</h1>
+      <DeviceInfo session={session} />
+    </div>
+  );
 }
