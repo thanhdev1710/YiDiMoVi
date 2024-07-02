8290103c9aa6074eac92d02b1316d207b496a9fe
Chí Thành <chithanh171004@gmail.com>
2024-07-02 18:48:39 +0700

    allowFullScreen

--- src/_components/VideoEmbed.tsx
+++ src/_components/VideoEmbed.tsx
@@ -2,6 +2,7 @@ export default async function VideoEmbed({ url }: { url: string }) {
   return (
     <section className="relative aspect-video w-full">
       <iframe
+        allowFullScreen
         src={url}
         className="absolute top-0 left-0 w-full h-full border-none"
       ></iframe>
